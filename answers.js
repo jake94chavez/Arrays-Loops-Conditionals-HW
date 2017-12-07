d45c7e7d7623a61f97b918ebda06600585294398
Jake Chavez <jake94chavez@gmail.com>
2017-12-06 23:41:43 -0600

    Commit 8 - I loops through their closets.

--- answers.js
+++ answers.js
@@ -56,3 +56,48 @@ const turts = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"]
 for (i=0; i < turts.length; i++) {
 	console.log(turts[i].toUpperCase());
 }
+
+// Return of the Closets
+
+const kristynsCloset = [
+  "left shoe",
+  "cowboy boots",
+  "right sock",
+  "GA hoodie",
+  "green pants",
+  "yellow knit hat",
+  "marshmallow peeps"
+];
+
+// Thom's closet is more complicated. Check out this nested data structure!!
+const thomsCloset = [
+  [
+    // These are Thom's shirts
+    "grey button-up",
+    "dark grey button-up",
+    "light blue button-up",
+    "blue button-up",
+  ],[
+    // These are Thom's pants
+    "grey jeans",
+    "jeans",
+    "PJs"
+  ],[
+    // Thom's accessories
+    "wool mittens",
+    "wool scarf",
+    "raybans"
+  ]
+];
+
+// Stuck here, Gotta come back to it
+
+// Dirty Laundry
+
+for (i=0; i < kristynsCloset.length; i++) {
+	console.log("WHIRR: Now wasing " + kristynsCloset[i])
+}
+
+for (i=0; i < thomsCloset.length; i++) {
+	console.log(thomsCloset[i])
+}
