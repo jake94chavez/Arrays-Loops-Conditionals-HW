5809b46e842dd063ae422bb5d6e964aee658f581
Jake Chavez <jake94chavez@gmail.com>
2017-12-06 23:07:04 -0600

    Commit 3 - Excited Kitten answered

--- answers.js
+++ answers.js
@@ -11,3 +11,16 @@ for (let i = 0; i <= 200; i++) {
 		console.log(i)
 	}
 }
+
+// Excited Kitten 
+let possible = [ "...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."]
+
+for (i=1; i <= 40; i++) {
+	if (i%2 != 0) {
+		console.log("Love me, pet me! HSSSSS!")
+	} else {
+		let min = Math.ceil(0);
+		let max = Math.floor(2);
+		console.log(possible[Math.floor(Math.random() * (max - min +1))]);
+	}
+}
